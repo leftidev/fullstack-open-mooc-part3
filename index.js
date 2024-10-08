9a4b439b6d55b4bec58e6cb0dfab67959936e617
leftidev <jes.bergstrom@gmail.com>
2024-10-08 06:45:01 +0300

    Implemented part 3.2.

--- index.js
+++ index.js
@@ -35,6 +35,13 @@ app.get('/api/persons', (request, response) => {
     response.json(persons)
 })
 
+app.get('/info', (request, response) => {
+    const numberOfPersons = persons.length;
+    const currentDate = new Date();
+    const responseText = `Phonebook has info for ${numberOfPersons} people<br/><br/>${currentDate}`;
+    response.send(responseText);
+});
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
