c87e489a28a27534ce182e9f6926d5ddbab94638
leftidev <jes.bergstrom@gmail.com>
2024-10-15 16:29:36 +0300

    Implemented part 3.16.

--- index.js
+++ index.js
@@ -5,16 +5,16 @@ const morgan = require('morgan');
 const cors = require('cors')
 const Person = require('./models/person')
 
-
-
-
-
 app.use(cors())
 app.use(express.static('dist'))
-
 app.use(express.json())
+
 morgan.token('body', (req) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
@@ -32,32 +32,20 @@ app.get('/info', (request, response) => {
     const responseText = `Phonebook has info for ${numberOfPersons} people<br/><br/>${currentDate}`;
     response.send(responseText);
 });
-/*
-app.get('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    const person = persons.find(person => person.id === id)
 
-
-    if (person) {
+app.get('/api/persons/:id', (request, response, next) => {
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
         response.json(person)
-    } else {
+      } else {
         response.status(404).end()
-    }
-})
-*/
-app.get('/api/persons/:id', (request, response) => {
-    Person.findById(request.params.id).then(person => {
-      response.json(person)
+      }
     })
-  })
-/*
-app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    persons = persons.filter(person => person.id !== id)
-  
-    response.status(204).end()
+
+    .catch(error => next(error))
 })
-*/
+
 app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
     .then(result => {
@@ -89,10 +77,24 @@ app.post('/api/persons', (request, response) => {
     
       person.save().then(savedPerson => {
         response.json(savedPerson)
-      }).catch(error => {
-        response.status(500).json({ error: 'failed to save person' });
-      });
+      })
 })
+
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+// this has to be the last loaded middleware, also all the routes should be registered before this!
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, '0.0.0.0', () => {
